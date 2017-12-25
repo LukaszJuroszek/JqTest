122b935ee6c4d7dcc832f5d9f7445e2dd15707ac
Łukasz Juroszek <lukjuroszek@gmail.com>
2017-12-25 13:18:22 +0100

    ADD: Add ef, context and simple person model

---
+++ JqTest/JqTest/DAL/JqContext.cs
@@ -0,0 +1,17 @@
+﻿using JqTest.Models;
+using System;
+using System.Data.Entity;
+
+namespace JqTest.DAL
+{
+    public class JqContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public JqContext()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory);
+            Database.SetInitializer(new CreateDatabaseIfNotExists<JqContext>());
+        }
+    }
+}
---
+++ JqTest/JqTest/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JqTest.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
